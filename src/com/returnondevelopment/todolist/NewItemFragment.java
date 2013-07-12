63569ee4aa69d6b9bd1d656775adf1cccd84386e
Seth Hayward <seth.hayward@gmail.com>
2013-07-12 10:01:33 -0700

    creating new UI fragement

---
+++ src/com/returnondevelopment/todolist/NewItemFragment.java
@@ -0,0 +1,19 @@
+package com.returnondevelopment.todolist;
+
+import android.app.Activity; 
+import android.app.Fragment;
+import android.view.KeyEvent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class NewItemFragment extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.new_item_fragment,  container, false);
+	}
+
+}
